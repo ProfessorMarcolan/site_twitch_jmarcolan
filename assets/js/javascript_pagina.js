a85f6880a0c4ffcff9eb7b38498f2abd62e4827a
joao_marcolan <jamarcolan@gmail.com>
2019-06-28 23:12:30 -0300

    Adicionando as contribuições do mooser :D

--- assets/js/javascript_pagina.js
+++ assets/js/javascript_pagina.js
@@ -8,19 +8,18 @@ class printando_na_tela{
     estruturar_elemento(element){
     let div = document.createElement("div")
     div.innerHTML =`
-    
+    <div class="col-sm-4">
     <div class="card">
-      <div class="card__img card__img--git">
-      <img src="${element.imagem_front}" class="image-fluid" width="300px"> 
-      </div>
+    <div class="card__img card__img--react"></div>
       <h2 class="card__title"><p><a href="${element.url}"> ${element.post_title} </a></p></h2>
       <p class="card__description">${element.post_des}</p>
       <div class="card__read"><a href="${element.url}"> Se aprochega! Vem conferir! </a></div>
     </div>
+    </div>
     
-    
-
     `
+
+    // style="background-image: url('${element.imagem_front}');"
 
     // <div> <p> Isso deve se tornar um cartao
     // <p/>
