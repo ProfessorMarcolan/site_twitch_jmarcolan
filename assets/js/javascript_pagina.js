8d334544cfa040f357ab1149fd588cbbccae1e9d
joao_marcolan <jamarcolan@gmail.com>
2019-06-28 22:45:24 -0300

    Primeiro produto viavel

--- assets/js/javascript_pagina.js
+++ assets/js/javascript_pagina.js
@@ -7,12 +7,26 @@ class printando_na_tela{
     // precisa melhorar a estrutura dos dados que v'ao para o div.innerHTML
     estruturar_elemento(element){
     let div = document.createElement("div")
-    div.innerHTML =`<div> <p> Isso deve se tornar um cartao
-    <p/>
-    <p><a href="${element.url}"> ${element.post} </a><p/>
-        <img src="${element.imagem_front}" alt="Minha Figura">	
+    div.innerHTML =`
+    
+    <div class="card">
+      <div class="card__img card__img--git">
+      <img src="${element.imagem_front}" class="image-fluid" width="300px"> 
+      </div>
+      <h2 class="card__title"><p><a href="${element.url}"> ${element.post_title} </a></p></h2>
+      <p class="card__description">${element.post_des}</p>
+      <div class="card__read"><a href="${element.url}"> Se aprochega! Vem conferir! </a></div>
     </div>
+    
+    
+
     `
+
+    // <div> <p> Isso deve se tornar um cartao
+    // <p/>
+    // <p><a href="${element.url}"> ${element.post} </a></p>
+    //     <img src="${element.imagem_front}" alt="Minha Figura">	
+    // </div>
 
     return div
 }
