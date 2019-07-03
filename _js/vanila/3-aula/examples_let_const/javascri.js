620fe49aaa06c14d443fc72f33d933fbb81b7c78
joao_marcolan <jamarcolan@gmail.com>
2019-07-03 00:28:03 -0300

    Escrevendo as aulas de javascript core.

--- _js/vanila/3-aula/examples_let_const/javascri.js
+++ _js/vanila/3-aula/examples_let_const/javascri.js
@@ -1,11 +1,27 @@
-console.log("asdsad")
+console.log("asdoi mundosad")
 
 
-function teste(){
-    
-    console.log(a)
-    let a = 12309123
-    
+const get_nome = async(numero) => {
+    let variavel = ["joao","lucas","bernardo"]
+
+    // setTimeout(()=>{},1000)
+    let i = 0
+    while(i<100000000){i++}
+   
+    return variavel[numero]
 }
 
-teste()
+
+
+const get_nome_by_id = async() =>{
+
+    var a = get_nome(1)
+    var b = get_nome(2)
+
+    return Promise.all([a,b])
+
+
+}
+
+
+console.log(get_nome_by_id())
